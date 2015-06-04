f604a0afe0ee0e6c178fec205f2c70fd05a8862d
zhuoxuan <zhuoxuan@mogujie.com>
2015-06-04 14:01:35 +0800

    提交微信工程文件

---
+++ 12-DemoForDev/weixin/wechat/src/main/java/com/zhuoxuan/wechat/servlet/WeChatServlet.java
@@ -0,0 +1,31 @@
+package com.zhuoxuan.wechat.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ */
+public class WeChatServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public WeChatServlet() {
+    }
+
+	/**
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
